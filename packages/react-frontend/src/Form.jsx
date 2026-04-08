422f3f12cb6fef94dd768165beaef39a3af85fd9
vkohli244 <Kohlivins@gmail.com>
2026-04-07 23:02:37 -0700

    feat: finished form with input fields for name and job

--- packages/react-frontend/src/Form.jsx
+++ packages/react-frontend/src/Form.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 
-function Form() {
+function Form(props) {
   const [person, setPerson] = useState({
     name: "",
     job: "",
@@ -8,31 +8,37 @@ function Form() {
 
   function handleChange(event) {
     const { name, value } = event.target;
-    if (name === "job") setPerson({ name: person.name, job: value });
-    else setPerson({ name: value, job: person.job });
+    if (name === "job") setPerson({ ...person, job: value });
+    else setPerson({ ...person, name: value });
   }
 
+  function submitForm() {
+    props.handleSubmit(person);
+    setPerson({ name: "", job: "" }); // this clears the form since we defined setPerson as a state setter for the const person which is what the input fields in the form represent
+  }
   return (
-  <form>
-    <label htmlFor="name">Name</label>
-    <input
-      type="text"
-      name="name"
-      id="name"
-      value={person.name}
-      onChange={handleChange}
-    />
-    <label htmlFor="job">Job</label>
-    <input
-      type="text"
-      name="job"
-      id="job"
-      value={person.job}
-      onChange={handleChange}
-    />
-  </form>
-);
+    <form>
+      <label htmlFor="name">Name</label>
+      <input
+        type="text"
+        name="name"
+        id="name"
+        value={person.name}
+        onChange={handleChange}
+      />
+      <label htmlFor="job">Job</label>
+      <input
+        type="text"
+        name="job"
+        id="job"
+        value={person.job}
+        onChange={handleChange}
+      />
+      <button type="button" onClick={submitForm}>
+        Submit
+      </button>
+    </form>
+  );
 }
-
 
 export default Form;
--- packages/react-frontend/src/MyApp.jsx
+++ packages/react-frontend/src/MyApp.jsx
@@ -10,21 +10,24 @@ function MyApp() {
   const [characters, setCharacters] = useState([]);
 
   function removeOneCharacter(index) {
-    const updated = characters.filter((character, i) => {
+    const updated = characters.filter((character, i) => { // again this is a new array "updated" we're not changing the array of characters defined as const above
       return i !== index; // if i == index that means this is the index of the row we want to delete
     });
     setCharacters(updated);
   }
 
+  function updateList(person) {
+    setCharacters([...characters, person]); // it's a shallow copy of the array using the spread operator ... which takes the old character array and then using "," appends the new person
+  }
   return (
     <div className="container">
-      <Table characterData={characters} removeCharacter ={removeOneCharacter}/>
-      <Form />
+      <Table characterData={characters} removeCharacter={removeOneCharacter} />
+      <Form handleSubmit={updateList} />  
+      
     </div>
-    
   );
-
-  
 }
 
 export default MyApp;
+
+// Line 25: prop handleSubmit calls the updateList function 
