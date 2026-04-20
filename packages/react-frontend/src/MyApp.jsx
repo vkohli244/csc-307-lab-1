c40423d6d77ed1f078cf529aaf07a83eeb67671b
vkohli244 <Kohlivins@gmail.com>
2026-04-20 09:51:44 -0700

    feat: Added POST route connection with frontend

--- packages/react-frontend/src/MyApp.jsx
+++ packages/react-frontend/src/MyApp.jsx
@@ -5,49 +5,63 @@ import Form from "./Form";
 
 // src/MyApp.jsx
 
+function postUser(person) {
+  const promise = fetch("Http://localhost:8000/users", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(person),
+  });
+
+  return promise;
+}
+
 function MyApp() {
   const [characters, setCharacters] = useState([]);
 
-  
   async function fetchUsers() {
-    try{
+    try {
       const response = await fetch("http://localhost:8000/users");
       const json = await response.json();
       return json.users_list;
-    }
-    catch(error){
+    } catch (error) {
       console.log(error);
       return false;
     }
   }
-  
-  useEffect(()=>{
-    fetchUsers().then(result=>{
-      if (result){
+
+  useEffect(() => {
+    fetchUsers().then((result) => {
+      if (result) {
         setCharacters(result);
       }
     });
-  },[]);
+  }, []);
 
-  
   function removeOneCharacter(index) {
-    const updated = characters.filter((character, i) => { // again this is a new array "updated" we're not changing the array of characters defined as const above
+    const updated = characters.filter((character, i) => {
       return i !== index; // if i == index that means this is the index of the row we want to delete
     });
     setCharacters(updated);
   }
 
   function updateList(person) {
-    setCharacters([...characters, person]); // it's a shallow copy of the array using the spread operator ... which takes the old character array and then using "," appends the new person
+    postUser(person)
+      .then(() => setCharacters([...characters, person]))
+      .catch((error) => {
+        console.log(error);
+      });
   }
+
   return (
     <div className="container">
       <Table characterData={characters} removeCharacter={removeOneCharacter} />
-      <Form handleSubmit={updateList} />  
-      
+      <Form handleSubmit={updateList} />
     </div>
   );
 }
+
 
 export default MyApp;
 
