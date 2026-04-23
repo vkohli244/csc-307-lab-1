4d47d3b4a5a2a85868cfc8459e23085349d32d17
vkohli244 <Kohlivins@gmail.com>
2026-04-22 17:34:12 -0700

    feat: added missed GET route from lab 2

--- packages/express-backend/backend.js
+++ packages/express-backend/backend.js
@@ -53,6 +53,20 @@ const findUser = (name, job) => {
   });
 };
 
+const findUserByName = (name) => {
+  return users["users_list"].filter((user) => user["name"] === name);
+};
+
+app.get("/users", (req, res) => {
+  const name = req.query.name;
+  if (name != undefined) {
+    let result = findUserByName(name);
+    result = { users_list: result };
+    res.send(result);
+  } else {
+    res.send(users);
+  }
+});
 
 
 app.get("/users", (req, res) => {
